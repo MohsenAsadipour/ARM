0c7882e967bf05e65c1b5edab669fa92eba66a21
Mohsen <mohsen.asadipour@fpt.de>
2017-10-25 21:46:34 +0200

    simple project

---
+++ MB1034B_A0_iotoggle0/.dep/stm32f0xx_it.o.d
@@ -0,0 +1,33 @@
+src\stm32f0xx_it.o: src\stm32f0xx_it.c inc/stm32f0xx_it.h \
+ ..\STM32F0-Discovery_FW_V1.0.0\Libraries\CMSIS\ST\STM32F0xx\Include/stm32f0xx.h \
+ ..\STM32F0-Discovery_FW_V1.0.0\Libraries\CMSIS\Include/core_cm0.h \
+ c:\yagarto-20121222\bin\../lib/gcc/arm-none-eabi/4.7.2/include/stdint.h \
+ c:/yagarto-20121222/lib/gcc/../../arm-none-eabi/sys-include/stdint.h \
+ ..\STM32F0-Discovery_FW_V1.0.0\Libraries\CMSIS\Include/core_cmInstr.h \
+ ..\STM32F0-Discovery_FW_V1.0.0\Libraries\CMSIS\Include/core_cmFunc.h \
+ ..\STM32F0-Discovery_FW_V1.0.0\Libraries\CMSIS\ST\STM32F0xx\Include/system_stm32f0xx.h \
+ inc/stm32f0xx_conf.h \
+ ..\STM32F0-Discovery_FW_V1.0.0\Libraries\STM32F0xx_StdPeriph_Driver\inc/stm32f0xx_gpio.h \
+ ..\STM32F0-Discovery_FW_V1.0.0\Libraries\STM32F0xx_StdPeriph_Driver\inc/stm32f0xx_rcc.h
+
+inc/stm32f0xx_it.h:
+
+..\STM32F0-Discovery_FW_V1.0.0\Libraries\CMSIS\ST\STM32F0xx\Include/stm32f0xx.h:
+
+..\STM32F0-Discovery_FW_V1.0.0\Libraries\CMSIS\Include/core_cm0.h:
+
+c:\yagarto-20121222\bin\../lib/gcc/arm-none-eabi/4.7.2/include/stdint.h:
+
+c:/yagarto-20121222/lib/gcc/../../arm-none-eabi/sys-include/stdint.h:
+
+..\STM32F0-Discovery_FW_V1.0.0\Libraries\CMSIS\Include/core_cmInstr.h:
+
+..\STM32F0-Discovery_FW_V1.0.0\Libraries\CMSIS\Include/core_cmFunc.h:
+
+..\STM32F0-Discovery_FW_V1.0.0\Libraries\CMSIS\ST\STM32F0xx\Include/system_stm32f0xx.h:
+
+inc/stm32f0xx_conf.h:
+
+..\STM32F0-Discovery_FW_V1.0.0\Libraries\STM32F0xx_StdPeriph_Driver\inc/stm32f0xx_gpio.h:
+
+..\STM32F0-Discovery_FW_V1.0.0\Libraries\STM32F0xx_StdPeriph_Driver\inc/stm32f0xx_rcc.h:
---
+++ MB1034B_A0_iotoggle0/.dep/stm32f0xx_rcc.o.d
@@ -0,0 +1,32 @@
+..\STM32F0-Discovery_FW_V1.0.0\Libraries\STM32F0xx_StdPeriph_Driver\src\stm32f0xx_rcc.o: \
+ ..\STM32F0-Discovery_FW_V1.0.0\Libraries\STM32F0xx_StdPeriph_Driver\src\stm32f0xx_rcc.c \
+ ..\STM32F0-Discovery_FW_V1.0.0\Libraries\STM32F0xx_StdPeriph_Driver\inc/stm32f0xx_rcc.h \
+ ..\STM32F0-Discovery_FW_V1.0.0\Libraries\CMSIS\ST\STM32F0xx\Include/stm32f0xx.h \
+ ..\STM32F0-Discovery_FW_V1.0.0\Libraries\CMSIS\Include/core_cm0.h \
+ c:\yagarto-20121222\bin\../lib/gcc/arm-none-eabi/4.7.2/include/stdint.h \
+ c:/yagarto-20121222/lib/gcc/../../arm-none-eabi/sys-include/stdint.h \
+ ..\STM32F0-Discovery_FW_V1.0.0\Libraries\CMSIS\Include/core_cmInstr.h \
+ ..\STM32F0-Discovery_FW_V1.0.0\Libraries\CMSIS\Include/core_cmFunc.h \
+ ..\STM32F0-Discovery_FW_V1.0.0\Libraries\CMSIS\ST\STM32F0xx\Include/system_stm32f0xx.h \
+ inc/stm32f0xx_conf.h \
+ ..\STM32F0-Discovery_FW_V1.0.0\Libraries\STM32F0xx_StdPeriph_Driver\inc/stm32f0xx_gpio.h
+
+..\STM32F0-Discovery_FW_V1.0.0\Libraries\STM32F0xx_StdPeriph_Driver\inc/stm32f0xx_rcc.h:
+
+..\STM32F0-Discovery_FW_V1.0.0\Libraries\CMSIS\ST\STM32F0xx\Include/stm32f0xx.h:
+
+..\STM32F0-Discovery_FW_V1.0.0\Libraries\CMSIS\Include/core_cm0.h:
+
+c:\yagarto-20121222\bin\../lib/gcc/arm-none-eabi/4.7.2/include/stdint.h:
+
+c:/yagarto-20121222/lib/gcc/../../arm-none-eabi/sys-include/stdint.h:
+
+..\STM32F0-Discovery_FW_V1.0.0\Libraries\CMSIS\Include/core_cmInstr.h:
+
+..\STM32F0-Discovery_FW_V1.0.0\Libraries\CMSIS\Include/core_cmFunc.h:
+
+..\STM32F0-Discovery_FW_V1.0.0\Libraries\CMSIS\ST\STM32F0xx\Include/system_stm32f0xx.h:
+
+inc/stm32f0xx_conf.h:
+
+..\STM32F0-Discovery_FW_V1.0.0\Libraries\STM32F0xx_StdPeriph_Driver\inc/stm32f0xx_gpio.h:
